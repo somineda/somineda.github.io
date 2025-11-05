5f542cd15753e30abb1efb2ee24d9d945718a113
somy <fuureelf@naver.com>
2025-11-05 16:51:25 +0900

    자기소개

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,11 @@ export const siteConfig = {
   social: {
     email: "fuurele@gmail.com",
     github: "https://github.com/somineda",
+    blog: "https://velog.io/@sommnie/posts"
   },
   aboutMe:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Python", "React", "Node.js", "Python", "AWS", "Docker"],
+    "저는 새로운 기술을 빠르게 학습하고 프로젝트에 직접 적용하는 개발자입니다. VGG16 기반 의류 추천 연구와 Sentence Transformers를 활용한 향수 추천 서비스를 통해 AI 추천 시스템의 전체 파이프라인을 경험했습니다. PostgreSQL, Redis, Celery를 활용해 데이터 처리 효율을 높이며 서비스 성능을 개선했습니다. LMS 프로젝트 팀장으로서 코드리뷰와 스크럼 문화를 주도해 협업 효율을 향상시켰습니다. 문제 발생 시 적극적으로 피드백을 주고받으며 팀 내 신뢰를 형성했습니다. 공식 문서 탐독과 멘토 피드백을 통해 꾸준히 학습하며 실무 감각을 키워왔습니다. 현재도 멘토와 동료 개발자들과 기술 트렌드를 공유하며 성장하고 있습니다. AI와 데이터로 사용자 경험을 개선하는 개발자가 되는 것이 제 목표입니다.",
+  skills: ["Python", "Django", "Flask", "PostgreSQL", "AWS", "Docker"],
   projects: [
     {
       name: "AI Dev Roundup Newsletter",
